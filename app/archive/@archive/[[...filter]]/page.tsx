91a89279d92161b8c2cf239c4d3ae83190ed7207
Adarsh Anmol <adarshanmol10@gmail.com>
2025-06-08 21:20:16 +0530

    5: added months links to the catch-all layout as well

--- app/archive/@archive/[[...filter]]/page.tsx
+++ app/archive/@archive/[[...filter]]/page.tsx
@@ -1,6 +1,6 @@
 import NewsList from "@/components/news-list";
 import Link from "next/link";
-import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
+import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/lib/news";
 import { NewsItem } from "@/types/news";
 
 //catc-all params will be an array of strings
@@ -15,9 +15,16 @@ export default async function FilteredNewsPage({params}: {params: Promise<{ filt
     const selectedMonth = filter?.[1];
 
     let news: NewsItem[] = [];
+    let links = getAvailableNewsYears();
 
     if (selectedYear && !selectedMonth){
         news = getNewsForYear(selectedYear);
+        links = getAvailableNewsMonths(selectedYear);
+    }
+
+    if (selectedYear && selectedMonth){
+        news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+        links = []; //set the links to disappear when month and year both selected
     }
 
     let defNewsContent = <p> No news for the specific period of time. </p>
@@ -27,17 +34,20 @@ export default async function FilteredNewsPage({params}: {params: Promise<{ filt
         defNewsContent = <NewsList news={news}/>
     }
 
-    const links = getAvailableNewsYears();
-
     return (
         <>
         <header id ="archive-header">
             <nav>
                 <ul>
-                    {links.map(link => <li key ={link}>
-                        <Link href = {`/archive/${link}`}>{link}</Link>
-                    </li>)}
-
+                    {links.map((link)=>{
+                        const href = selectedYear ? `/archive/${selectedYear}/${link}` : `archive/${link}`
+                        //changing the link based on if month selected or not
+                        return(
+                            <li key = {link}>
+                                <Link href={href}>{link}</Link>
+                            </li>
+                        )
+                    })}
                 </ul>
             </nav>
 
