eff39e4e57793440dea34b4361ce72b803b912da
Adarsh Anmol <adarshanmol10@gmail.com>
2025-06-08 14:38:04 +0530

    1: added basic pages and added dynamic news page with images

---
+++ app/news/[slug]/page.tsx
@@ -0,0 +1,21 @@
+import { DUMMY_NEWS } from "@/dummy-news";  
+
+
+export default async function NewsDetailPage({params}: {params: Promise<{ slug: string }>}) {
+  
+    const newsSlug = (await params).slug;
+    const newsItem =  DUMMY_NEWS.find(newsItem => newsItem.slug === newsSlug) ;
+
+    return (
+    <>
+      <article className="news-article">
+        <header>
+          <img src = {`/images/news/${newsItem?.image}`} alt={newsItem?.title}/>
+          <br/>
+          <time dateTime={newsItem?.date}>{newsItem?.date}</time>
+        </header>
+      <p>News: {newsItem?.content}</p>
+      </article>
+    </>
+  );
+}
---
+++ app/news/page.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { DUMMY_NEWS } from "@/dummy-news";
+
+export default function NewsPage() {
+  return (
+    <div id="news">
+      <h1>The News</h1>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href= {`/news/${newsItem.slug}`}>
+            <img src= {`/images/news/${newsItem.image}`} alt= {newsItem.title}></img>
+            </Link>
+            <span>
+              {newsItem.title}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
