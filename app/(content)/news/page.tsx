55391d6b183cb75d0c191c2b3145773a755355c9
Adarsh Anmol <adarshanmol10@gmail.com>
2025-06-09 14:51:14 +0530

    13: added client-side fetching

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,11 +1,52 @@
-import { DUMMY_NEWS } from "@/dummy-news";
+"use client";
+
 import NewsList from "@/components/news-list";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+
+  const [error, setError] = useState<string>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [news, setNews] = useState();
+
+  //not a good practice to make the main useEffect function async, so make another async function for fetch
+  //fetch return a promise, therefore await 
+  useEffect(()=>{
+    setIsLoading(true);
+    async function fetchNews(){
+      const response = await fetch('http://localhost:8080/news');
+
+      if(!response.ok){
+        setError('Failed to fetch News!')
+        setIsLoading(false);
+      }
+
+      const news = await response.json();
+      setIsLoading(false);
+      setNews(news);
+    }
+   
+    
+    fetchNews();
+  }, []);
+
+  if(isLoading){
+    return <p>Loading...</p>
+  }
+  if(error){
+    return <p>{error}</p>
+  }
+  let newsContent;
+
+  if(news){
+    newsContent = <NewsList news={news}/>
+  }
+
   return (
     <div id="news">
       <h1>The News</h1>
-      <NewsList news={DUMMY_NEWS}/>
+      {newsContent}
+      
     </div>
   );
 }
