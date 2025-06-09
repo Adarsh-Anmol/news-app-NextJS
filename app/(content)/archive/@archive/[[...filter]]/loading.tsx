14da5d09231d20e673593553b59be1a4d31ce8a5
Adarsh Anmol <adarshanmol10@gmail.com>
2025-06-09 21:55:54 +0530

    14: added delay and loading page to app

---
+++ app/(content)/archive/@archive/[[...filter]]/loading.tsx
@@ -0,0 +1,5 @@
+export default function ArchiveLoading(){
+    return (
+        <p>Archived News Loading</p>
+    )
+}
---
+++ app/(content)/archive/@latest/loading.tsx
@@ -0,0 +1,5 @@
+export default function ArchiveLoading(){
+    return (
+        <p>Latest News Loading</p>
+    )
+}
